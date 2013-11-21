b3663d12e81eb02361887ee6ec8dd8abc59f0ab1
Juhani <ja222qm@lnu.student.se>
2013-11-21 09:22:32 +0200

    vs sync

---
+++ Tria/UnitTestTria/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestTria
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void isIsoscelesTest()
+        {
+            Triangle tria = new Triangle(2.2, 3.2, 2.2);
+            Assert.IsTrue(tria.isIsosceles());
+        }
+    }
+}
