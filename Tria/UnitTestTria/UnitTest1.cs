e9007924a7f8b22abe8389ff66e14af7b60ec707
Juhani <ja222qm@lnu.student.se>
2013-11-21 11:45:32 +0200

    Test för liksidighet, likbenhet och olika sidor

--- Tria/UnitTestTria/UnitTest1.cs
+++ Tria/UnitTestTria/UnitTest1.cs
@@ -12,5 +12,19 @@ namespace UnitTestTria
             Triangle tria = new Triangle(2.2, 3.2, 2.2);
             Assert.IsTrue(tria.isIsosceles());
         }
+
+        [TestMethod]
+        public void isEquilateralTest()
+        {
+            Triangle tria = new Triangle(2.1, 3.2, 4.2);
+            Assert.IsTrue(tria.isEquilateral());
+        }
+        
+        [TestMethod]
+        public void isScaleneTest()
+        {
+            Triangle tria = new Triangle(3.4, 3.4, 3.4);
+            Assert.IsTrue(tria.isScalene());
+        }
     }
 }
